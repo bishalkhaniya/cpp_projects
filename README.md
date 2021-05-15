ea49ffebcdddb2d2b189313bc1968cf70f9e3a87
Bishal Khaniya <bishalkhaniya0404@gmail.com>
2021-05-14 22:26:26 -0700

    second commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# cpp_projects
-Labworks of C++
---
+++ bis.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+    int a,b;
+    cout<<"enter any two number:"<<endl;
+    cin>>a>>b;
+    cout<<"sum is:"<<a+b;
+    cin>>a;
+    return 0;
+}   
