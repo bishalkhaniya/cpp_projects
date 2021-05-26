31929e78c96eaec3a7f6d056521fb84ca5b51545
Bishal Khaniya <bishalkhaniya0404@gmail.com>
2021-05-26 12:57:21 +0545

    implementing structure

--- aot.cpp
+++ aot.cpp
@@ -1,9 +1,13 @@
 #include<iostream> 
-int main(){ float height,base,area; 
+int main()
+{ 
+    float height,base,area; 
 std::cout<<"Enter base"<<std::endl; 
 std::cin>>base;
  std::cout<<"Enter heightside b"<<std::endl; 
  std::cin>>height;
   area=(base*height)*0.5;
   std::cout<<"Area of triangle is: "<<area<<std::endl; 
-return 0; }
+std::cin>>base;
+return 0;
+ }
