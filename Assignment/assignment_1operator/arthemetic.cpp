614f969a7736740a710af5582d5a180b7d6057d3
Bishal Khaniya <bishalkhaniya0404@gmail.com>
2021-06-04 14:45:30 +0545

    Arthemetic Operator

--- Assignment/assignment_1operator/arthemetic.cpp
+++ Assignment/assignment_1operator/arthemetic.cpp
@@ -3,15 +3,15 @@
 #include<iostream>
 int main(){
   int number1=10,number2=20;
-  int result =number1+number2;  // + operator(addition operator)
+  int result =number1+number2;  //  operator(addition operator)
   std::cout<<"Sum is : "<<result<<std::endl;
-  result =number2/number1;      // / operator(division operator)
+  result =number2/number1;      //  operator(division operator)
   std::cout<<"Division is : "<<result<<std::endl;
-  result =number2*number1;      // * operator(Multiplication operator)
+  result =number2*number1;      //  operator(Multiplication operator)
   std::cout<<"Multiplication is : "<<result<<std::endl;
-  result =number1-number2;      // - operator(subtraction operator)
+  result =number1-number2;      //  operator(subtraction operator)
   std::cout<<"Subtraction is : "<<result<<std::endl;
-  result =number2 % number1;      // % operator(Modulus operator)
+  result =number2 % number1;      //  operator(Modulus operator)
   std::cout<<"Remainder is : "<<result<<std::endl;
   return 0;
 }
