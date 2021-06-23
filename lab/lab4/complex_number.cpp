dc5bd7cc4d741b15587515a4d043ba465fce1393
Bishal Khaniya <bishalkhaniya0404@gmail.com>
2021-06-23 13:14:35 +0545

    passing onj as argument

--- lab/lab4/complex_number.cpp
+++ lab/lab4/complex_number.cpp
@@ -1,40 +1,33 @@
 #include<iostream>
 using namespace std;
-
-
-class Complex
+class complex1
 {
-	private:
-		float real;
-		float imag;
-	public:
-		
-		Complex(){
-			real = 0;
-			imag = 0;
-		}
-		
-		Complex(float r, float i){
-			real = r;
-			imag = i;
-		}
-		
-		Complex add(Complex c){
-			
-			return Complex(real + c.real, imag + c.imag);
-		} 
-	
-		void display(){
-			cout<<"("<<real<<","<<imag<<")"<<endl;
-		}
+    private:
+    int real,imz;
+    public:
+    void input()
+    {
+        cout<<"enter the real and imaginary part of complex no"<<endl;
+        cin>>real>>imz;
+    }
+    complex1 addcomplex(complex1 c1,complex1 c2)
+    {
+        complex1 temp;
+        temp.real=c1.real+c2.real;
+        temp.imz=c1.imz+c2.imz;
+        return(temp);
+    }
+    void display()
+    {
+        cout<<"the summation is"<<endl<<real<<"+"<<"i"<<imz<<endl;
+    }
 };
-
-int main(){
-	Complex c1(2, 5.5), c2(3.3, 4), c3;
-	c3 = c1.add(c2);
-	c1.display();
-	c2.display();
-	cout<<"And the sum is :"<<endl;
-	c3.display();
-	return 0;
+int main()
+{
+    complex1 c1,c2,c3;
+    c1.input();
+    c2.input();
+    c3=c3.addcomplex(c1,c2);//passing object as function argument
+    c3.display();
+    return(0);
 }
